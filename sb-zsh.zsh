ab9b1dcf1554ee83a1f4ad2081966f0982ef367c
Kory Hoopes <kory@seabear.dev>
2024-03-10 23:13:52 -0600

    Clarified some of the documentation for sb-zsh.zsh

--- sb-zsh.zsh
+++ sb-zsh.zsh
@@ -1,6 +1,4 @@
 #!/bin/zsh
-# File: load_scripts.zsh
-# Description: This script loads all the .zsh files in the $HOME/.sb-zsh/scripts/ directory.
 
 # Define the directory where the scripts are located
 scripts_dir="$HOME/.sb-zsh/scripts"
