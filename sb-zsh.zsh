af38752e7d2d282098f3c403bbbc255a19ac1c25
Kory Hoopes <kory@seabear.dev>
2024-03-10 23:10:03 -0600

    Simplified and added documentation to the sb-zsh.zsh script.

--- sb-zsh.zsh
+++ sb-zsh.zsh
@@ -1,7 +1,15 @@
 #!/bin/zsh
+# File: load_scripts.zsh
+# Description: This script loads all the .zsh files in the $HOME/.sb-zsh/scripts/ directory.
 
-for script in "$HOME/.sb-zsh/scripts/"*.zsh; do
+# Define the directory where the scripts are located
+scripts_dir="$HOME/.sb-zsh/scripts"
+
+# Loop through all the .zsh files in the scripts directory
+for script in "$scripts_dir/"*.zsh; do
+  # Check if the file exists and is a regular file
   if [[ -f "$script" ]]; then
+    # Source the script file
     source "$script"
   fi
 done
