37385f2328073af3dbed51ee5b1d258fc5522a68
Kory Hoopes <kory@seabear.dev>
2024-03-10 23:06:00 -0600

    Simplified and added documentation to the install.zsh script.

--- install.zsh
+++ install.zsh
@@ -1,26 +1,38 @@
 #!/bin/zsh
 
-echo "Install sb-zsh scripts to '$HOME/.sb-zsh'? (y/n)"
-read response
+# Function to install sb-zsh scripts
+install_sb_zsh() {
+  local install_dir="$HOME/.sb-zsh"
 
-if [[ "$response" == "y" ]]; then
-  if [[ ! -d "$HOME/.sb-zsh" ]]; then
-    echo "Directory does not exist. Creating it..."
-    mkdir "$HOME/.sb-zsh"
-  else
-    echo "The directory already exists, deleting and recreating it..."
-    rm -rf "$HOME/.sb-zsh"
-    mkdir "$HOME/.sb-zsh"
+  # Check if the directory exists, remove it if it does
+  if [[ -d "$install_dir" ]]; then
+    echo "The directory already exists, deleting it..."
+    rm -rf "$install_dir"
   fi
 
-  echo "Installing scripts to $HOME/.sb-zsh..."
-  cp ./sb-zsh.zsh "$HOME/.sb-zsh"
-  cp -r ./scripts* "$HOME/.sb-zsh"
+  # Create the directory
+  echo "Creating directory '$install_dir'..."
+  mkdir "$install_dir"
 
+  # Copy the scripts to the directory
+  echo "Installing scripts to $install_dir..."
+  cp ./sb-zsh.zsh "$install_dir"
+  cp -r ./scripts* "$install_dir"
+
+  # Source the .zshrc file
   echo "Scripts installed successfully, sourcing the .zshrc file..."
   source "$HOME/.zshrc"
 
   echo "Sourcing complete."
+}
+
+# Prompt the user for confirmation
+echo "Install sb-zsh scripts to '$HOME/.sb-zsh'? (y/n)"
+read response
+
+# Check the user's response and install if confirmed
+if [[ "$response" == "y" ]]; then
+  install_sb_zsh
 else
   echo "Installation cancelled."
 fi
